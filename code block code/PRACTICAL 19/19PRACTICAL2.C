3be09890937159b8a6f147f424564177a98378b5
Zeelgadara05 <zeelgadara05@gmail.com>
2025-09-08 23:57:08 +0530

    Add files via upload

---
+++ code block code/PRACTICAL 19/19PRACTICAL2.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int i,j;
+
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=9;j++)
+		{
+			if(j==i||j==(2*5-i))
+				printf("%d",j);
+			else if(i==1)
+				printf("%d",j);
+			else
+				printf(" ");
+		}
+		printf("\n");
+
+	}
+	return 0;
+
+}
+
+
+
+
+
