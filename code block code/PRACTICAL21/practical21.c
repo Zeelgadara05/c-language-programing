3dcf7a05fa94e28b8fbf3c216a0a15c105c70598
Zeelgadara05 <zeelgadara05@gmail.com>
2025-11-10 13:58:49 +0530

    Update practical21.c

--- code block code/PRACTICAL21/practical21.c
+++ code block code/PRACTICAL21/practical21.c
@@ -1,40 +1,27 @@
 #include <stdio.h>
-
-int main() {
+void main()
+{
     int numbers[25];
-    int i;
-    int positiveCount = 0, negativeCount = 0;
-    int evenCount = 0, oddCount = 0;
-
+    int i,positivecount=0,negativecount=0,evencount=0,oddcount=0;
     printf("Enter 25 integers:\n");
-
-    // Input 25 integers
-    for (i = 0; i < 25; i++) {
+    for (i=0;i<25;i++)
+    {
         printf("Enter number %d: ", i + 1);
-        scanf("%d", &numbers[i]);
+        scanf("%d",&numbers[i]);
     }
-
-    // Process each number
-    for (i = 0; i < 25; i++) {
-        // Count positive and negative numbers
-        if (numbers[i] > 0)
-            positiveCount++;
-        else if (numbers[i] < 0)
-            negativeCount++;
-
-        // Count even and odd numbers
-        if (numbers[i] % 2 == 0)
-            evenCount++;
+    for (i = 0; i < 25; i++)
+    {
+        if (numbers[i]>0)
+            positivecount++;
+        else if (numbers[i]<0)
+            negativecount++;
+        if (numbers[i]%2==0)
+            evencount++;
         else
-            oddCount++;
+            oddcount++;
     }
-
-    // Display results
-    printf("\n--- Data Statistics ---\n");
-    printf("Total Positive Numbers: %d\n", positiveCount);
-    printf("Total Negative Numbers: %d\n", negativeCount);
-    printf("Total Even Numbers: %d\n", evenCount);
-    printf("Total Odd Numbers: %d\n", oddCount);
-
-    return 0;
+    printf("Total Positive Numbers: %d\n", positivecount);
+    printf("Total Negative Numbers: %d\n", negativecount);
+    printf("Total Even Numbers: %d\n", evencount);
+    printf("Total Odd Numbers: %d\n", oddcount);
 }
