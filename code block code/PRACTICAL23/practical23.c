a033018170e209223c48d17808d6ae14a055a8db
Zeelgadara05 <zeelgadara05@gmail.com>
2025-11-10 14:07:20 +0530

    Update practical23.c

--- code block code/PRACTICAL23/practical23.c
+++ code block code/PRACTICAL23/practical23.c
@@ -1,35 +1,23 @@
 #include <stdio.h>
-
-int main() {
-    int n, i;
-
-    printf("Enter number of days: ");
-    scanf("%d", &n);
-
-    int prices[n];
-
-    printf("Enter fruit prices for %d days:\n", n);
-    for (i = 0; i < n; i++) {
-        printf("Day %d price: ", i + 1);
-        scanf("%d", &prices[i]);
+void main()
+{
+    int n,i,profit,prices[n],maxprofit=0;
+    int minprice=prices[0];
+    printf("Enter number of days:");
+    scanf("%d",&n);
+    printf("Enter fruit prices for %d days:\n",n);
+    for(i=0;i<n;i++)
+    {
+        printf("Day %d price:",i+1);
+        scanf("%d",&prices[i]);
     }
-
-    int minPrice = prices[0];
-    int maxProfit = 0;
-
-    // Traverse through the price list
-    for (i = 1; i < n; i++) {
-        // Check if selling today gives better profit
-        int profit = prices[i] - minPrice;
-        if (profit > maxProfit)
-            maxProfit = profit;
-
-        // Update the minimum price if current price is lower
-        if (prices[i] < minPrice)
-            minPrice = prices[i];
+    for (i=1;i<n;i++)
+    {
+        profit=prices[i]-minprice;
+        if (profit>maxprofit)
+            maxprofit=profit;
+        if (prices[i]<minprice)
+            minprice=prices[i];
     }
-
-    printf("\nMaximum Profit: %d\n", maxProfit);
-
-    return 0;
+    printf("\nMaximum Profit: %d\n", maxprofit);
 }
